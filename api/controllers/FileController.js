15894670ddb2e85ae0ee5f150ae08e414e5a5d9b
pranavrajs <pranavrajs@gmail.com>
2014-11-28 21:54:32 +0530

    Upload function modified as on new Sails version

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -8,17 +8,27 @@
 module.exports = {
 	upload: function  (req, res) {
 		if(req.method === 'GET')
-			return res.json({'status':'GET not allowed'});						//	Call to /upload via GET is error
+			return res.json({'status':'GET not allowed'});						
+		//	Call to /upload via GET is error
 
 		var uploadFile = req.file('uploadFile');
 		console.log(uploadFile);
+		
+		//Use this to upload to custom folder
+		//If you don't want this remove {dirname: ''}
+		//There are other options also .Check at skipper docs
 
-	    uploadFile.upload({ dirname: './assets/images'},function onUploadComplete (err, files) {				//	Files will be uploaded to .tmp/uploads
-	    																		
-	    	if (err) return res.serverError(err);								//	IF ERROR Return and send 500 error with error
+		//If dirname is not set the upload will be done to ./tmp/uploads	
+	    uploadFile.upload({ dirname: '../../assets/images'},function onUploadComplete (err, files) {				
+	    // Files will be uploaded to /assets/images/
+	    // Access the files via localhost:1337/images/yourfilename
+
+	    	if (err) return res.serverError(err);								
+	    	//	IF ERROR Return and send 500 error with error
 			
 	    	console.log(files);
 	    	res.json({status:200,file:files});
+	    	//This will print the details including new file name upload path etc
 	    });
 	},
 };
